ebe62a676adff7f28edd2f091574e29c987824a6
aweer <aweer@DESKTOP-CGSHU5P>
2021-05-18 20:09:13 +0530

    update project in Time Table management

---
+++ src/IT19031408/PrepareStatement.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IT19031408;
+
+/**
+ *
+ * @author Hashini Piyathissa
+ */
+class PrepareStatement {
+    
+}
