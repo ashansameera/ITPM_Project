58521c07856772df49e77f791e626343a43ea544
Hashini Piyathissa <Hashini Piyathissa@DESKTOP-CM6T2PR>
2021-05-20 19:05:58 +0530

    UPDATED Parallel

--- src/IT19031408/Parallel.java
+++ src/IT19031408/Parallel.java
@@ -231,7 +231,7 @@ public class Parallel extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
